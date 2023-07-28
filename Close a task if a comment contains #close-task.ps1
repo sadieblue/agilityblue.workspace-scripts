f1289a5db390be19312e0257cfc7bb0698842744
Agility Blue <support@agilityblue.com>
2023-07-28 04:50:03 -0500

    added a workspace script

---
+++ Close a task if a comment contains #close-task.ps1
@@ -0,0 +1,35 @@
+if ($null -eq $agilityBlueEvent) {
+  # If the event object is not available, it means we are executing the script manually.
+  # In this case, we'll set an id directly for testing purposes
+  $taskCommentId = 22178
+} else {
+  # The script was executed by an event, so we'll have access to the event object
+  $taskCommentId = $agilityBlueEvent.Payload.Id
+}
+
+# Here, we are retrieving the comment and expanding the task so we can access task properties.
+$taskComment = Get-TaskComment $taskCommentId -ExpandTask
+
+# We can uncomment this line to output the properties/values available to us
+# $taskComment | ConvertTo-JSON
+
+# First, we want to make sure that the task isn't already closed
+if ($taskComment.Task.StatusDescription -in "Completed", "Canceled") {
+  # The task is already closed out, so do nothing
+  Write-Output "Task $($taskComment.TaskId) is $($taskComment.Task.StatusDescription). No action will be performed."
+  Exit
+}
+
+# We're interested in the "Value" property here, specifically if it contains
+# the text "#close-task"
+if ($taskComment.Value -like "*#close-task*") {
+  Write-Output "Comment $($taskComment.TaskCommentId) on task $($taskComment.TaskId) contains a #close-task command. Closing the task..."
+  
+  # Here, we issue a complete task command and set the Task property to the result
+  $taskComment.Task = Set-CompleteTask $taskComment.TaskId
+  
+  # The Set-CompleteTask cmdlet will return a task object that we can use for output
+  Write-Output "Task $($taskComment.TaskId) is now $($taskComment.Task.StatusDescription)"
+} else {
+  Write-Output "Comment $($taskComment.TaskCommentId) on task $($taskComment.TaskId) does not contain a #close-task command. No action will be performed."
+}
